df0c95e973f67c4fcdc58d5a41e997a3f15de245
smorchkova001-git <smorchkova001@gmail.com>
2025-12-21 21:27:21 +0300

    Change main.py and router.py

--- src/app/router.py
+++ src/app/router.py
@@ -3,10 +3,11 @@ from fastapi.params import Depends
 from pydantic import BaseModel
 
 from app.dependencies import get_toxicity_service, get_session
+from app.auth.dependencies import get_current_user, require_admin
 from app.service import ToxicityService
 from .models.base import ToxicityType
+from app.database import User
 from sqlalchemy.orm import Session
-
 from datetime import datetime
 
 
@@ -31,24 +32,23 @@ router = APIRouter()
 
 
 @router.post("/forward")
-def forward(message: MessageData, 
-            toxicity_service: ToxicityService = Depends(get_toxicity_service)):
+def forward(message: MessageData,
+           user: User = Depends(get_current_user), # тут будет ошибка если нет авторизации
+           toxicity_service: ToxicityService = Depends(get_toxicity_service)):
     return ToxicityReport(toxicity_type=toxicity_service.get_toxicity_type(message.text))
 
-
 @router.get("/history", response_model=list[ForwardCallSchema])
-def get_history(toxicity_service: ToxicityService = Depends(get_toxicity_service)):
-    # TODO: реализовать получение истории
+def get_history(toxicity_service: ToxicityService = Depends(get_toxicity_service),
+               user: dict = Depends(require_admin)): # ошибка если пользователь не админ
     return toxicity_service.get_history()
 
-
 @router.delete("/history")
-def delete_history(toxicity_service: ToxicityService = Depends(get_toxicity_service)):
-    # TODO: реализовать удаление истории
+def delete_history(toxicity_service: ToxicityService = Depends(get_toxicity_service),
+                  user: dict = Depends(require_admin)): # ошибка если пользователь не админ
     return toxicity_service.delete_history()
 
-
 @router.get("/history/stats")
-def get_stats(toxicity_service: ToxicityService = Depends(get_toxicity_service)):
+def get_stats(toxicity_service: ToxicityService = Depends(get_toxicity_service),
+             user: dict = Depends(require_admin)): # ошибка если пользователь не админ
     # TODO: реализовать получение статистики
     return toxicity_service.get_stats()
